238486037dac046b868b88b2ee8ed2ab6d7d3dad
Octobitss <aerontarroza5@gmail.com>
2025-07-26 16:04:19 +0800

    changes in docker

--- resources/js/api.js
+++ resources/js/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:8000/api";
+const API_URL = "https://batang-inocencio.onrender.com/api";
 
 // const API_URL = "https://trecebatanginocencio.site/api";
 
