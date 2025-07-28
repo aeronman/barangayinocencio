81f1071e8b6b387a71af1080409fda62aab16240
Octobitss <aerontarroza5@gmail.com>
2025-07-28 17:15:49 +0800

    adjust

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,8 @@
 // vite.config.js
+import { defineConfig } from 'vite';
+import laravel from 'laravel-vite-plugin';
+import react from '@vitejs/plugin-react';
+
 export default defineConfig({
   base: '/build/',
   build: {
