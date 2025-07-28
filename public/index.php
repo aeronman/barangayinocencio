0eba22795f7e6c7fe7fcacf835151efb6e5d5c77
Octobitss <aerontarroza5@gmail.com>
2025-07-28 17:54:43 +0800

    move index to public

---
+++ public/index.php
@@ -0,0 +1,21 @@
+<?php
+
+use Illuminate\Foundation\Application;
+use Illuminate\Http\Request;
+
+define('LARAVEL_START', microtime(true));
+
+// Maintenance file path
+if (file_exists($maintenance = __DIR__ . '/../storage/framework/maintenance.php')) {
+    require $maintenance;
+}
+
+// Composer autoloader
+require __DIR__ . '/../vendor/autoload.php';
+
+// Laravel bootstrap
+$app = require_once __DIR__ . '/../bootstrap/app.php';
+
+$app->handleRequest(
+    Illuminate\Http\Request::capture()
+);
