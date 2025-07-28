56784df08e5792b08c0785fb0c7f99e4bdec1a25
Octobitss <aerontarroza5@gmail.com>
2025-07-28 17:48:54 +0800

    update server.php

--- vendor/laravel/framework/src/Illuminate/Foundation/resources/server.php
+++ vendor/laravel/framework/src/Illuminate/Foundation/resources/server.php
@@ -20,4 +20,5 @@ $remoteAddress = $_SERVER['REMOTE_ADDR'].':'.$_SERVER['REMOTE_PORT'];
 
 file_put_contents('php://stdout', "[$formattedDateTime] $remoteAddress [$requestMethod] URI: $uri\n");
 
-require_once $publicPath.'/index.php';
+require_once __DIR__.'/index.php';
+
